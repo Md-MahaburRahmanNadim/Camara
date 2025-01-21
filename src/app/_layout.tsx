aa9b9964b610b136759732c5295713f27ae70b8a
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2025-01-21 19:10:11 +0600

    adding camera functionality

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,10 @@ const CameraScreen = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }}></Stack.Screen>
+      <Stack.Screen
+        name="camera"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
     </Stack>
   );
 };
--- src/app/camera.tsx
+++ src/app/camera.tsx
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from "react-native";
+import { Link, router } from "expo-router";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
 const CameraScreen = () => {
+  const [facing, setfacing] = useState<CameraType>("back");
+  const switingCamera = () => {
+    setfacing((current) => (current === "back" ? "front" : "back"));
+  };
+  const [permission, requestPermission] = useCameraPermissions();
+  if (permission && !permission.granted && permission.canAskAgain) {
+    requestPermission();
+  }
+  if (!permission?.granted) {
+    return <ActivityIndicator />;
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Camara screen</Text>
-      <Link href="/">Home</Link>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.footer}>
+          <MaterialIcons
+            name="flip-camera-android"
+            size={30}
+            color="white"
+            onPress={switingCamera}
+          ></MaterialIcons>
+        </View>
+      </CameraView>
+      <MaterialIcons
+        name="close"
+        color="white"
+        size={30}
+        style={styles.closeIcon}
+        onPress={router.back}
+      ></MaterialIcons>
     </View>
   );
 };
@@ -21,5 +56,22 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "500",
     color: "green",
+  },
+  camera: {
+    width: "100%",
+    height: "100%",
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+  },
+  footer: {
+    marginTop: "auto",
+    padding: 20,
+    paddingBottom: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#00000099",
   },
 });
